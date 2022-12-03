d72cb44e5d9294770c798b32a20f7b7f7820d758
Alex Yu <alexy23@g.ucla.edu>
2022-12-02 22:24:50 -0800

    Solve 2022 day 3

--- 2022/day-03/answer.py
+++ 2022/day-03/answer.py
@@ -1,16 +1,34 @@
 import argparse
 
+def calculate_priority(c):
+    if c.islower():
+        return ord(c) - ord('a') + 1
+    else:
+        return ord(c) - ord('A') + 27
+
 def part_a(filename):
     print('Trying part a...')
     with open(filename) as f:
         lines = f.read().splitlines()
-        pass
+        s = 0
+        for line in lines:
+            l = len(line)
+            a, b = line[:l//2], line[l//2:]
+            shared = set(a).intersection(set(b))
+            s += calculate_priority(list(shared)[0])
+        print(s)
 
 def part_b(filename):
     print('Trying part b...')
     with open(filename) as f:
         lines = f.read().splitlines()
-        pass
+        s = 0
+        for i in range(len(lines)//3):
+            a, b, c = lines[3*i:3*(i+1)]
+            shared = set(a).intersection(set(b)).intersection(set(c))
+            c = list(shared)[0]
+            s += calculate_priority(list(shared)[0])
+        print(s)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', help='the input file')
