5a95b6d167036adf2efc4266beb42358788ba278
Alex Yu <yu.alexanderx@gmail.com>
2024-12-18 21:29:03 -0800

    Make sure bootstrap script doesn't overwrite existing files

--- 2024/bootstrap.sh
+++ 2024/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+
 real_day=$(date +%d)
 aoc_day=$(( (real_day % 30) + 1 ))
 if [ $# -gt 1 ]; then
@@ -13,14 +14,14 @@ if [ $aoc_day -lt 10 ]; then
     fday="0${aoc_day}"
 fi
 
-echo "Creating template for day-${fday}"
-
-if [[ -d "day-${fday}" ]]; then
-    echo "Directory for day-${fday} already exists"
-    exit 1
-fi
+print -P "%F{green}Creating template for day-${fday}...%f"
 
 for fname in template/*; do
-    newname=$(echo "$fname" | sed "s/{{aoc_day}}/${aoc_day}/g")
-    cat "$fname" | sed "s/{{aoc_day}}/${aoc_day}/g" > "$(basename $newname)"
+    newpath=$(echo "$fname" | sed "s/{{aoc_day}}/${aoc_day}/g")
+    newfname=$(basename $newpath)
+    if [[ -e $newfname ]]; then
+        print -P "%F{red}File '${newfname}' already exists!%f"
+        continue
+    fi
+    cat "$fname" | sed "s/{{aoc_day}}/${aoc_day}/g" > "${newfname}"
 done
